7702512ce19b0be52537e27f94a2d41b4237cb3e
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2025-10-14 01:21:22 -0300

    add: Funcionalidades de listar e detalhar posts com templates e  tambem criacao do template base

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listar_posts, name='home'),
+    path('detalhar_post/<int:pk>/', views.detalhar_post, name='detalhar_post'),
+    path('adicionar_comentario/<int:pk>/', views.adicionar_comentario, name='adicionar_comentario')
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def listar_posts(request):
+     posts = Post.objects.all()
+     return render(request, 'listar_posts.html', context= {
+          'posts': posts
+     })
+
+def detalhar_post(request, pk):
+     post = Post.objects.get(pk = pk)
+     return render(request, "detalhar_post.html", context = {
+          "post": post
+     })
+
+def criar_post(request):
+     ...
+
+def editar_post(request):
+     ...
+
+def deletar_post(request):
+     ...
+
+def adicionar_comentario(request):
+     ...
